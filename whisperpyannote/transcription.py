e072567367132be6dab21e14374137dde0ca4d0b
Marc Delage <marcdelage@MacBook-Air-M1-de-Marc.local>
2025-09-19 13:07:57 +0200

    Suppression de tous les fichiers

--- whisperpyannote/transcription.py
+++
@@ -1,6 +0,0 @@
-import whisper
-
-def transcribe(audio_path, model_size="turbo"):
-    model = whisper.load_model(model_size)
-    result = model.transcribe(audio_path)
-    return result["segments"]
